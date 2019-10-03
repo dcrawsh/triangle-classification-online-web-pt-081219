10deddfd46eb4906d43885f12668f41edfb77a29
Dylan Crawshaw <dcrawsh@gmail.com>
2019-10-03 18:43:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  
+  def initialize(side_one,side_two,side_three)
+  @side_one = side_one
+  @side_two = side_two
+  @side_three = side_three
+  end 
+  
+  def kind 
+  if @side_one == @side_two and @side_one == @side_three
+  :equilateral
+  elsif @side_one != @side_two and @side_one != @side_three and @side_three != @side_one
+  :scalene 
+  elsif @side_one != @side_two and @side_two == @side_three
+  :isosceles
+end 
+ 
+  end 
+  
 end
